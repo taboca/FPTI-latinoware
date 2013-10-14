942b1df4199839dfd22d301698849825850bd3b0
marcio <taboca@gmail.com>
2013-10-14 18:56:33 -0300

    twit

--- static/const.js
+++ static/const.js
@@ -1,4 +1,4 @@
 URL_FEED = '/channel/identica.xml'
 URL_FOTOS = '/channel/latinoware.json'
-URL_TWIT = "/channel/fisl.txt";
+URL_TWIT = "/channel/twit.txt";
 URL_DESTAQUES = "/channel/destaques.xml";
