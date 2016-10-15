869f0c12f58c83921ed3513f0cbe0f258d676ee9
Marcio <taboca@gmail.com>
2016-10-15 18:01:20 -0300

    Fixes config

--- browser/browser_right.js
+++ browser/browser_right.js
@@ -1,8 +1,8 @@
 var config = {
    'page'   : 'http://www.telasocial.com:8888/static/wrappers/right.html',
-   'width'  : 1920, 
-   'height' : 1080
+   'width'  : 1920,
+   'height' : 1080,
+   'frame'  : false
 };
 
 module.exports = config;
-
