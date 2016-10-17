cba16e666cd4055d16b48871ded0db2fe2683dcf
Marcio <taboca@gmail.com>
2016-10-17 19:00:22 -0200

    polish

--- static/const.js
+++ static/const.js
@@ -1,5 +1,6 @@
-URL_FEED = '/channel/identica.xml'
-URL_FOTOS = '/channel/fotos.txt'
-URL_TWIT = "/channel/twitter.txt";
-URL_NOTICIAS = "/channel/destaques.xml";
+URL_FEED      = '/channel/identica.xml';
+URL_FOTOS     = '/channel/fotos.txt';
+URL_TWIT      = '/channel/twitter.txt';
+URL_NOTICIAS  = "/channel/destaques.xml";
+
 TEMPO_REFRESH = 10000;
