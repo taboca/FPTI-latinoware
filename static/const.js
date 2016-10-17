f9e66aa96f8f3e8ae12e85c06ba27521edd284ed
Marcio <taboca@gmail.com>
2016-10-17 18:59:40 -0200

    Twit is new URL

--- static/const.js
+++ static/const.js
@@ -1,5 +1,5 @@
 URL_FEED = '/channel/identica.xml'
 URL_FOTOS = '/channel/fotos.txt'
-URL_TWIT = "/channel/latinoware.txt";
+URL_TWIT = "/channel/twitter.txt";
 URL_NOTICIAS = "/channel/destaques.xml";
 TEMPO_REFRESH = 10000;
