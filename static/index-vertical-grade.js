84cb743e150a5a43d105fd284ef6c12d1139c38c
Marcio <taboca@gmail.com>
2016-10-18 19:45:12 -0200

    Grade

--- static/index-vertical-grade.js
+++ static/index-vertical-grade.js
@@ -1,5 +1,3 @@
-/* Grids On The Fly */
-
 $(document).ready(function() {
 
     setTimeout('startEngine()',1000);
@@ -9,46 +7,7 @@ $(document).ready(function() {
     register("/main/topheader" , "topo"  , "./header-vertical/index.html", iframeTemplate);
     register("/main/hora"      , "hora"  , "./tempo/index.html", iframeTemplate);
     register("/main/data"      , "data"  , "./tempo/date.html", iframeTemplate);
+    register("/main/sectionC"   , "fHardware" , "./grade-hardware/index.html", iframeTemplate);
     compile();
 
 });
-
-function startEngine() {
-   s1();
-//   setTimeout("cicleMidia()",TEMPO_INICIO_MIDIA);
-}
-
-function cicleMidia() {
-   setTimeout( function () {
-	var doc = $("#main #middle #abas").get();
-	doc = document.getElementById("meio").contentDocument;
-	cc.send( doc.getElementById("galeria").contentDocument, "container", "rotate");
-	cicleMidia();
-   }, TEMPO_REFRESH_MIDIA);
-}
-
-function s1() {
-  if(document.location.toString().indexOf("mode")>-1) {
-    var param = document.location.toString().split("mode=");
-    if(param[1]=="tv") {
-      //document.getElementById("viewport").style.width="1080";
-      //document.getElementById("viewport").style.height="1920";
-      animate();
-    }
-  }
-}
-
-var i =0;
-function animate() {
-  if(i==0) {
-    //$('#screen1').attr('style','transition-property:transform;transition-duration:4s; transform:translate(0,-70px) rotateY(-360deg) rotateX(-360deg) rotateZ(360deg);transform-origin:700px 100px  ')
-    i=1;
-  } else {
-    //$('#screen1').attr('style','transition-property:transform;transition-duration:4s; transform:translate(0,-70px) rotateY(0deg) rotateX(0deg) rotateZ(0deg);transform-origin:700px 100px  ')
-    i=0;
-  }
-  //tv.setup();
-  //tv.add($('#animation li'));
-  //tv.play();
-  setTimeout("animate()",TEMPO_REFRESH);
-}
