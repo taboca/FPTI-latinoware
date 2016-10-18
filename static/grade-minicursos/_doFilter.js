d79f7ac5e5ba54d82b69cd8b9b8d572163892d34
Marcio <taboca@gmail.com>
2016-10-18 19:49:56 -0200

    Minicursos voltou sem mini banners

--- static/grade-minicursos/_doFilter.js
+++ static/grade-minicursos/_doFilter.js
@@ -11,8 +11,8 @@ function doFilter(str, authorContainer) {
           authorProposal = gridMaker.candidates[authorContainer][0];
       }
   }
-  htmlMarkup = '<span class="zone" style="display:block;height:100%;background:url(http://www.mgalli.com/marcio.jpg)"><span class="author" >'+authorProposal+'</span>';
-  htmlMarkup+= '<div class="topShade" >'
+  htmlMarkup = '<span class="zone" style="display:block;height:100%;"><span class="author" >'+authorProposal+'</span>';
+  htmlMarkup+= '<div class="topShade_null" >'
   var opacity = 9;
   for (var i=0;i<s.length;i++) {
       var el = s[i];
